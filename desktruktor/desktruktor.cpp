a6b819edefddb7b947b84bf5757e95d7386bb1c4
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-13 13:48:11 +0700

    destruktor

---
+++ desktruktor/desktruktor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //construktor
+	~angka(); // destruktor
+	void cetakData();
+	void isiData();
+};
+
+//definisi member function
+angka::angka(int i) {
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+angka::~angka() {
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << " alamat arry sudah dilepaskan " << endl;
+}
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3);
+	angka* ptrBelajarcpp = new angka(5);
+	delete ptrBelajarcpp;
+
+	return 0;
+}
