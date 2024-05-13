29b6dbd02489b25ec3d655468c5369c614430794
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-13 13:40:15 +0700

    Add project files.

---
+++ Constructor_0131/Constructor_0131.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa					//class
+{
+public:							//akses modifier
+	static int nim;				//parameter
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
+};
+
+int mahasiswa::nim = 10;
+
+void mahasiswa::setID()					// set id
+{
+	id = ++nim;							//nim bernilai 0 ditambahkan 1 baru diberikan ke ID 
+}
