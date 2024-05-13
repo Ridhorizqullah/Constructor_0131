d894d6383e23cf51bc8f855c29eaa9f0217812ff
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-13 13:41:41 +0700

    membuat int main

--- Constructor_0131/Constructor_0131.cpp
+++ Constructor_0131/Constructor_0131.cpp
@@ -24,3 +24,26 @@ void mahasiswa::setID()					// set id
 	id = ++nim;							//nim bernilai 0 ditambahkan 1 baru diberikan ke ID 
 
 }
+
+void mahasiswa::printALL()
+{
+	cout << "ID = " << id << endl;
+	cout << "nama = " << nama << endl;
+	cout << endl;
+}
+
+int main()									//untuk memanggi;
+{
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbi");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+
+	return 0;
+
+}
